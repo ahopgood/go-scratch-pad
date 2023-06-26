2e59c3a205f0775b5e7493fe9316b81053e1cd56
Alex Hopgood <alex.hopgood@gmail.com>
2023-06-26 15:09:15 +0100

    Adding test suite

---
+++ maths/fibonacci_test.go
@@ -0,0 +1,56 @@
+package maths_test
+
+import (
+	"com/alexander/scratch/maths"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var fibonacci func() int
+
+var _ = Describe("Fibonacci", func() {
+
+	BeforeEach(func() {
+		fibonacci = maths.Fibonacci()
+	})
+
+	Context("when on one iteration", func() {
+		It("returns 0", func() {
+			Expect(fibonacci()).To(Equal(0))
+		})
+	})
+
+	Context("when on second iteration", func() {
+		It("returns 1", func() {
+			fibonacci()
+			Expect(fibonacci()).To(Equal(1))
+		})
+	})
+
+	Context("when on third iteration", func() {
+		It("returns 1", func() {
+			fibonacci()
+			fibonacci()
+			Expect(fibonacci()).To(Equal(1))
+		})
+	})
+
+	Context("when on fourth iterations", func() {
+		It("returns 2", func() {
+			fibonacci()
+			fibonacci()
+			fibonacci()
+			Expect(fibonacci()).To(Equal(2))
+		})
+	})
+
+	Context("when on fifth iterations", func() {
+		It("returns 3", func() {
+			fibonacci()
+			fibonacci()
+			fibonacci()
+			fibonacci()
+			Expect(fibonacci()).To(Equal(3))
+		})
+	})
+})
